169472c55e931e5ff5427107c37cdafaff097e93
Alex Marriott <marriottaog@gmail.com>
2021-08-23 14:43:47 +0100

    started process for conditional rendering

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import NewTask from './components/NewTask';
 
 export default function App() {
+  const [newTaskView, setNewTaskView] = useState(true)
+
   const addTaskHandler = (title, body) => {
     // Add to db, must retrieve from DB on open as well
     console.log(`title: ${title} body: ${body}`)
   };
 
-  return (
-    <View style={styles.container}>
+  if (newTaskView) {
+    return(
       <NewTask onAddTask={addTaskHandler}></NewTask>
-    </View>
-  );
+    )
+  } else {
+    return(
+      <Text>Hey</Text>
+    )
+  }
+
+  // return (
+  //   <View style={styles.container}>
+  //     <NewTask onAddTask={addTaskHandler}></NewTask>
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
