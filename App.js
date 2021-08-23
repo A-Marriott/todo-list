976e52c39b22a4c9048fe68ea9b74f2e037be05d
Alex Marriott <marriottaog@gmail.com>
2021-08-23 11:58:52 +0100

    added basic new task component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import NewTask from './components/NewTask';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hey!</Text>
+      <NewTask></NewTask>
     </View>
   );
 }
