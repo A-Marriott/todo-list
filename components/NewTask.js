f11e5dc569cb5046eabaf586e59cd01b9d5f9256
Alex Marriott <marriottaog@gmail.com>
2021-08-24 11:00:20 +0100

    good progress styling new task page

--- components/NewTask.js
+++ components/NewTask.js
@@ -16,40 +16,66 @@ const NewTask = (props) => {
 
   return (
     <View>
-      <Button
-      onPress={handleViewChange}
-      title="Back"
-      color="#841584"
-    />
-    <TouchableOpacity
-      onPress={handleViewChange}
-    >
-      <Text style={styles.backButton}>{"<"} Back</Text>
-    </TouchableOpacity>
-      <Text>Add a task</Text>
-      <Text>Title</Text>
-      <TextInput style={{height: 40}}
-        onChangeText={currentText => setTitle(currentText)}
-        value={title}/>
-      <Text>Body</Text>
-      <TextInput style={{height: 40}}
-        onChangeText={currentText => setBody(currentText)}
-        value={body}/>
-      <Button title="Submit" onPress={handleSubmit}/>
+      <TouchableOpacity
+        onPress={handleViewChange}
+      >
+        <Text style={styles.backButton}>{"<"} Back</Text>
+      </TouchableOpacity>
+      <View style={styles.addTaskContainer}>
+        <Text style={styles.marginBottom}>Add a task</Text>
+        <Text style={[styles.marginBottom, styles.boldText]}>Title</Text>
+        <TextInput style={[styles.marginBottom, styles.inputBoxTitle]}
+          onChangeText={currentText => setTitle(currentText)}
+          value={title}/>
+        <Text style={[styles.marginBottom, styles.boldText]}>Body</Text>
+        <TextInput style={[styles.marginBottom, styles.inputBoxBody
+          ]}
+          onChangeText={currentText => setBody(currentText)}
+          value={body}/>
+        <TouchableOpacity onPress={handleSubmit}>
+          <Text style={styles.submitButton}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  marginBottom: {
+    marginBottom: 12
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
   backButton: {
-
+    paddingLeft: 16,
+    paddingTop: 12,
+    fontWeight: "200"
   },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  addTaskContainer: {
+    paddingTop: 100,
+    padding: 24
   },
+  inputBoxTitle: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderStyle: 'solid',
+  },
+  inputBoxBody: {
+    height: 120,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderStyle: 'solid',
+  },
+  submitButton: {
+    height: 40,
+    color: 'white',
+    backgroundColor: '#9FB6B1',
+    lineHeight: 40,
+    textAlign: 'center',
+    borderRadius: 4
+  }
 });
 
 
