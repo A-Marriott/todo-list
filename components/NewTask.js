102b72a179900b488e19d9a8131c348a80f24efc
Alex Marriott <marriottaog@gmail.com>
2021-08-24 13:02:10 +0100

    added logic to stop submission without both title and body filled

--- components/NewTask.js
+++ components/NewTask.js
@@ -6,10 +6,25 @@ const NewTask = (props) => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [urgent, setUrgent] = useState(false);
+  const [titleEmptyOnSubmit, setTitleEmptyOnSubmit] = useState(false);
+  const [bodyEmptyOnSubmit, setBodyEmptyOnSubmit] = useState(false);
 
   const handleSubmit = () => {
-    props.onAddTask(title, body, urgent)
-    handleViewChange()
+    let allowSubmit = true;
+    setBodyEmptyOnSubmit(false)
+    setTitleEmptyOnSubmit(false)
+    if (!title) {
+      setTitleEmptyOnSubmit(true)
+      allowSubmit = false
+    }
+    if (!body) {
+      setBodyEmptyOnSubmit(true)
+      allowSubmit = false
+    }
+    if (allowSubmit) {
+      props.onAddTask(title, body, urgent)
+      handleViewChange()
+    }
   }
 
   const handleViewChange = () => {
@@ -30,12 +45,11 @@ const NewTask = (props) => {
       <View style={styles.addTaskContainer}>
         <Text style={styles.marginBottom}>Add a task</Text>
         <Text style={[styles.marginBottom, styles.boldText]}>Title</Text>
-        <TextInput style={[styles.marginBottom, styles.inputBoxTitle]}
+        <TextInput style={[styles.marginBottom, styles.inputBoxTitle, titleEmptyOnSubmit && styles.redBorder]}
           onChangeText={currentText => setTitle(currentText)}
           value={title}/>
         <Text style={[styles.marginBottom, styles.boldText]}>Body</Text>
-        <TextInput style={[styles.marginBottom, styles.inputBoxBody
-          ]}
+        <TextInput style={[styles.marginBottom, styles.inputBoxBody, bodyEmptyOnSubmit && styles.redBorder]}
           onChangeText={currentText => setBody(currentText)}
           value={body}/>
         <View style={{display: 'flex', flexDirection: 'row', marginTop: 10, marginBottom: 120}}>
@@ -87,6 +101,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  redBorder: {
+    borderColor: 'red'
   }
 });
 
