8d27e13ad80b1a933262879ebecfe31c72bee7c9
Alex Marriott <marriottaog@gmail.com>
2021-08-25 13:21:49 +0100

    added modal for deleting an item

--- components/TaskCard.js
+++ components/TaskCard.js
@@ -1,13 +1,43 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Pressable } from 'react-native';
 
 const TaskCard = (props) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const handleDelete = (id) => {
     props.handleDelete(props.id);
   };
 
   return(
     <View style={styles.container}>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Delete task?</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Pressable
+                style={styles.button}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.modalButtonText}>No</Text>
+              </Pressable>
+              <Pressable
+                style={styles.button}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                  handleDelete();
+                }}
+              >
+                <Text style={styles.modalButtonText}>Yes</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
       <View style={[styles.taskNumberIcon, props.urgent && styles.urgentBorder]}>
         <Text style={{color: 'white'}}>Task {props.taskNumber}</Text>
       </View>
@@ -15,7 +45,7 @@ const TaskCard = (props) => {
         <Text style={styles.title}>{props.title}</Text>
         <Text>{props.body}</Text>
       </View>
-      <TouchableOpacity style={styles.deleteButton} onPress={handleDelete}>
+      <TouchableOpacity style={styles.deleteButton} onPress={() => setModalVisible(true)}>
         <Text style={{fontWeight: 'bold', fontSize: 20}}>✖</Text>
       </TouchableOpacity>
     </View>
@@ -59,6 +89,39 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 0,
     right: 7
+  },
+  blank: {
+
+
+
+
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 20,
+    alignItems: 'center',
+    elevation: 3
+  },
+  button: {
+    borderRadius: 5,
+    padding: 8,
+    margin: 10,
+    width: 50,
+    backgroundColor: '#9FB6B1'
+  },
+  modalButtonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  modalText: {
+    marginBottom: 10,
+    textAlign: 'center'
   }
 });
 
